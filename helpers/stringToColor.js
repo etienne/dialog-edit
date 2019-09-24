d3c8362d937631eaf65e82be040cfe0b2eafd776
Étienne Després <etienne@pochour.com>
2019-09-24 18:12:18 -0400

    Handle undefined strings in stringToColor

--- helpers/stringToColor.js
+++ helpers/stringToColor.js
@@ -1,6 +1,10 @@
 // Taken from https://github.com/stuneak/create-color/
 
 export default function stringToColor(string) {
+  if (!string) {
+    return '#000000';
+  }
+
   const s = JSON.stringify(string);
   const hash = s.split("").reduce((a, _, i) => (a += s.charCodeAt(i) + (a << 5)), 0);
   return `#${(hash & 0x00ffffff).toString(16)}`;
