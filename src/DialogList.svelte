b63458831963baae43f7bd85bda92403491592a4
Étienne Després <etienne@pochour.com>
2021-05-02 12:29:08 -0400

    Initial Svelte version

---
+++ src/DialogList.svelte
@@ -0,0 +1,20 @@
+<script>
+  import ButtonIcon from './ButtonIcon.svelte';
+  import DialogItem from './DialogItem.svelte';
+  import { dialogs } from './stores.js';
+
+  let ids;
+  $: ids = Object.keys($dialogs);
+</script>
+
+<ul>
+  {#if ids.length}
+    {#each ids as id}
+      <li>
+        <DialogItem label={$dialogs[id].label}/>
+      </li>
+    {/each}
+  {/if}
+</ul>
+
+<ButtonIcon action={() => dialogs.add()} label="New Dialog"/>
---
+++ src/stores.js
@@ -0,0 +1,24 @@
+import { writable } from 'svelte/store';
+
+function createDialogs() {
+	const { subscribe, set, update } = writable({});
+
+	return {
+		subscribe,
+    add: () => update(d => {
+      const newId = getNewId(Object.keys(d));
+      d[newId] = { id: newId, label: 'untitled dialog' };
+      console.log('d=', d);
+      return d;
+    }),
+	};
+}
+
+function getNewId(ids) {
+  if (ids && Array.isArray(ids) && ids.length) {
+    return ids.reduce((maxId, id) => Math.max(id, maxId), -1) + 1;
+  }
+  return 1;
+}
+
+export const dialogs = createDialogs();
