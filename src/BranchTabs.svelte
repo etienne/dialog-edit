8a6c14f32a4d7557b4efd8600a482e4db17d1175
Étienne Després <etienne@pochour.com>
2021-09-28 16:53:07 -0400

    Allow branches to be reordered

--- src/BranchTabs.svelte
+++ src/BranchTabs.svelte
@@ -1,14 +1,34 @@
 <script>
+  import { flip } from 'svelte/animate';
+  import { dndzone } from 'svelte-dnd-action'
   import { nodes, selectLinkFromNode } from './stores/nodes';
   import Button from './Button.svelte';
   export let node = {};
+  const flipDurationMs = 200;
+  const dropTargetStyle = {};
+  let items = [], selectedBranchId;
+  $: items = node.branchTo.map(id => ({ id }));
+  $: selectedBranchId = node.branchTo[node.selectedBranch];
+
+  function handleDndConsider(e) {
+		items = e.detail.items;
+	}
+
+  function handleDndFinalize(e) {
+    const id = e.detail.info.id;
+    items = e.detail.items;
+		$nodes[node.id].branchTo = items.map(i => i.id);
+    $nodes[node.id].selectedBranch = node.branchTo.indexOf(id);
+	}
 
   function merge() {
     nodes.mergeNodes(node.id, node.branchTo[0]);
   }
 
-  function selectBranch(index) {
+  function selectBranch(id) {
+    const index = node.branchTo.indexOf(id);
     nodes.selectBranch(node.id, index);
+    console.log('clitkckt with index', index, 'node.branchTo', node.branchTo);
   }
 
   function addSibling() {
@@ -21,61 +41,61 @@
   }
 </script>
 
-<ul>
-  {#each node.branchTo as id, index}
-    <li class="tab" class:selected={(!node.selectedBranch && index == 0) || node.selectedBranch == index}>
-      <button on:click={() => selectBranch(index)}>
-        {($nodes[id].lines[0] && $nodes[id].lines[0].text) || 'empty branch'}
+<section>
+  <div use:dndzone={{items, flipDurationMs, dropTargetStyle, type: 'branch'}} on:consider={handleDndConsider} on:finalize={handleDndFinalize}>
+    {#each items as item, index (item.id)}
+      <button animate:flip={{duration: flipDurationMs}} on:click={() => selectBranch(item.id)} class:selected={(!node.selectedBranch && index == 0) || selectedBranchId == item.id}>
+        {($nodes[item.id] && $nodes[item.id].lines[0] && $nodes[item.id].lines[0].text) || 'empty branch'}
       </button>
-    </li>
-  {/each}
+    {/each}
+  </div>
   {#if !$selectLinkFromNode}
-    <li class="actions">
+    <aside>
       {#if node.branchTo.length === 1}
         <Button action={merge} label="Merge Nodes" icon="merge" block/>
       {/if}
       <Button action={deleteBranch} label="Delete Branch" icon="trash" block/>
       <Button action={addSibling} label="Add Branch" icon="plus" block/>
-    </li>
+    </aside>
   {/if}
-</ul>
+</section>
 
 <style>
-  ul {
+  section {
     display: flex;
-    border-radius: 8px;
+  }
+
+  div {
+    display: flex;
     font-size: 0.75em;
   }
 
-  li.tab {
+  button {
     margin-right: 0.6em;
-  }
-
-  li.tab button {
     max-width: 10em;
     overflow: hidden;
     background-color: var(--lightest-color);
     padding: 0.3rem 0.6rem;
-    border: 1px solid var(--lighter-color);
+    border: 1px solid var(--lightest-color);
     border-radius: 5px;
     white-space: nowrap;
     text-overflow: ellipsis;
     cursor: pointer;
   }
 
-  li.tab button:hover {
+  button:hover {
     background-color: var(--light-color);
   }
 
-  li.tab.selected button {
+  button.selected {
     background-color: var(--bg-color);
     border: 1px solid var(--lighter-color);
     box-shadow: 0 3px 18px 0 var(--light-color);
   }
 
-  li.actions {
+  aside {
     display: flex;
     margin-left: auto;
-    padding: 0.1rem;
+    /* padding: 0.1rem; */
   }
 </style>
